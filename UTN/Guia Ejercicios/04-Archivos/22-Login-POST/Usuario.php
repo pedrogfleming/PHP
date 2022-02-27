49dc1c0e35e484d380942702cf189dad3a0e264f
Geraghty Fleming Pedro <pedrogfleming@gmail.com>
2022-02-27 00:09:16 -0300

    ejercicio 22 resuelto

---
+++ UTN/Guia Ejercicios/04-Archivos/22-Login-POST/Usuario.php
@@ -0,0 +1,61 @@
+<?php
+    enum ELogin: int{
+        case Verificado = 1;
+        case ErrorDatos = 2;
+        case Inexistente = 3;
+        };
+Class Usuario{
+    private string $_nombre;
+    private string $_mail;
+    private string $_clave;
+
+    function __construct(string $nombre,string $mail,string $clave){
+        $this->_nombre = $nombre;
+        $this->_mail = $mail;
+        $this->_clave = $clave;
+    }
+
+    public function MostrarDatos(){
+        return $this->_nombre."/".$this->_mail."/".$this->_clave;
+    }
+    static function SaveFile(string $data,string $path="usuarios",bool $append=false,string $endFormat=".csv"){
+        $ret = false;
+        if(!Usuario::IsNullOrEmptyString($data)){
+            $file;
+            $path .= $endFormat;
+            if($append){
+                $file = fopen($path,"a");
+                $data = "\r\n".$data;                             
+            }
+            else{
+                $file = fopen($path,"w");                    
+            }
+            if(fwrite($file,$data) > 0){
+                $ret = true;  
+            }
+        }
+        fclose($file);
+        return $ret;
+    }
+
+    private static function IsNullOrEmptyString($str){
+        return ($str === null || trim($str) === '');
+    }
+    /**
+     * @return true si los dos usarios son iguales
+     * @return false si no coincide el mail
+     * @return -1 si coincide el mail pero no la clave
+     * return bool
+     */
+    public static function Verificar(Usuario $u1,Usuario $u2):ELogin{
+        if($u1->_mail == $u2->_mail){
+            if($u1->_clave == $u2->_clave){                 
+                return ELogin::Verificado;
+            }
+            return ELogin::ErrorDatos;
+        }
+        return ELogin::Inexistente;
+    }
+}
+
+?>
---
+++ UTN/Guia Ejercicios/04-Archivos/22-Login-POST/action.php
@@ -0,0 +1,50 @@
+<?php
+require_once("Usuario.php");
+if(isset($_POST['nombre']) &&
+ isset($_POST['mail']) &&
+ isset($_POST['clave'])) {    
+    $nombre = $_POST['nombre'];
+    $mail = $_POST['mail'];
+    $clave = $_POST['clave'];
+    $usuario = new Usuario($nombre,$mail,$clave);
+    $usuarios = [];
+    $file = fopen("usuarios.csv","r");
+    $line;
+    $retStr = "";
+    while(!feof($file)) {
+        $line = fgets($file);
+        // echo $line . "<br>";
+        if(!$line){
+            echo("No se pudo leer correctamente el archivo");
+            break;
+        }
+        else{
+            $auxStr = [];
+            //Instancio objetos Usuarios x cada linea
+            //formato>>> /nombre/mail/clave/
+            foreach (explode("/",$line) as $item) {
+                str_replace('/','',$item);
+                array_push($auxStr,$item);
+            }
+            $u = new Usuario($auxStr[0],$auxStr[1],$auxStr[2]);
+            if($u != null){
+                $verificacion = Usuario::Verificar($u,$usuario);
+                switch ($verificacion) {
+                    case ELogin::Verificado:
+                        echo("Verificado,los datos ingresados son correctos");
+                        return;
+                    case ELogin::ErrorDatos:
+                        echo("Error en los datos, revise la clave");
+                        return;
+                        break;
+                    case ELogin::Inexistente:                        
+                        break;
+                }                 
+               
+            }
+            
+        }
+    }
+    echo("Usuario no registrado");
+}
+?>
